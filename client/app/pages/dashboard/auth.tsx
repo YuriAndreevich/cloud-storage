6c8d0ab24fe948eb2133f60924774dd956b3da43
Yuri Silin <try2scumme@gmail.com>
2023-07-11 15:26:09 +0300

    first commit

---
+++ client/app/pages/dashboard/auth.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { LoginForm } from '@/components/auth/LoginForm'
+import { NextPage } from 'next'
+
+export const AuthPage: NextPage = () => {
+    return (
+        <LoginForm />
+    )
+}
