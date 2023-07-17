d7a57f33f6f27d1cadaa06f438ef362b442dc98b
SilinYuri <yuriandreev11ch@gmail.com>
2023-07-18 02:05:50 +0300

    1

--- client/app/dashboard/auth/page.tsx
+++ client/app/dashboard/auth/page.tsx
@@ -1,6 +1,17 @@
+import { LoginForm } from "@/app/components/auth/LoginForm";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const AuthPage: NextPage = () => {
-  return <div> AuthPage </div>;
+  return (
+    <>
+      <Head>
+        <title> Dashboard / Auth</title>
+      </Head>
+      <main>
+        <LoginForm />
+      </main>
+    </>
+  );
 };
 export default AuthPage;
