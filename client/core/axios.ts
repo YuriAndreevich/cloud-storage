053835466cbd3aa17972fd11a98caf1a73f34329
SilinYuri <yuriandreev11ch@gmail.com>
2023-07-18 04:36:33 +0300

    1

---
+++ client/core/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import { parseCookies } from 'nookies'
+ 
+axios.default.baseURL = 'http://localhost:3001';
+
+axios.interceptors.request.use((config) => {
+    if (typeof window !== 'undefined') {
+        const { _token } = parseCookies()
+        config.headers.Authorization = `Bearer ${_token}`
+})
